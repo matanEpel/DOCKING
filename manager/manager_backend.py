481828da5752003df97e9f95265ce6e2f42f69ed
matanepel <matan.epel12@gmail.com>
2021-05-25 23:19:03 +0300

    finishing writing the api

--- manager/manager_backend.py
+++ manager/manager_backend.py
@@ -1,14 +1,56 @@
-# TODO: def add_drive_data(drive_key)
-# creates a connection to the data base (drive) based on the drive directory key
+# TODO: def add_person(name, email)
+#  adding a person to the list of "docers" by it's name and email
 
-# TODO: def get_list_of_docs(search_string)
-# gets a list of docs names and keys out of the drive by a given search input
+# TODO: def del_person(name)
+#  deleting a person from the list of "docers"
 
-# TODO: def get_doc(doc_key)
-# gets the dock by its key
+# TODO: def get_persons()
+#  returns the list of all the "docers" names and emails
 
-# TODO: def add_doc(file_path)
-# adds the dock to the drive and all the tags we need
+# TODO: def add_point(name)
+#  adds 1 "docing" point to the given name
 
-# TODO: def add_desc(doc_key, desc)
-# adds desc (tags) to the doc to make the search better
+# TODO: def del_point(name)
+#  deletes 1 "docing" point to the given name
+
+# TODO: def get_docer():
+#  returns the name and email of the next person who need to document
+
+# TODO: def add_calender(calender_id)
+#  adds a calender that should be documented to the list of calenders
+
+# TODO: def get_calenders()
+#  return the calenders ids and names of the documented calenders
+
+# TODO: def add_non_activity(activity name)
+#  adds a name to the list of activities that should not be documented
+
+# TODO: def del_non_activity(name)
+#  deletes the activity from the list of activities that should not be documented
+
+# TODO: def add_to_drive(activity_name)
+#  adds a directory to the drive with the basic "docing" format
+
+# TODO: def del_from_drive(name)
+#  deletes an activity folder from the drive
+
+# TODO: def get_non_activity()
+#  gets the list of all the activities that should not be documented
+
+# TODO: def create_k_events(calender_id, k)
+#  creates the next k events in the given calender (send calender inventation and creates drive foldr)
+
+# TODO: def get_events(calender_id, k)
+#  return the next k events we need to doc in a given calender
+
+# TODO: def	add_event(name)
+#  create event in the calender and opens the folder in the drive
+
+# TODO: def del_event(name)
+#  deletes an event from the docking calender and the folder from the drive
+
+# TODO: def get_non_done(calender_id)
+#  gets a list of all the events that hasn't been documented but should have
+
+# TODO: def remind_not_done(calender_id)
+#  sends email to all the people who hasnt documented the event they should have
