130e63fed75615b8b7cdf8b9d37f73593bcf574f
matanepel <matan.epel12@gmail.com>
2021-05-26 22:28:13 +0300

    adding function declerations

---
+++ manager/vershkov_stuff.py
@@ -0,0 +1,53 @@
+def add_person(name, email):
+    """
+    adds a person to the file of persons
+    """
+    pass
+
+
+def del_person(name):
+    """
+    deletes the person from the list of persons
+    """
+
+
+def get_persons():
+    """
+    return all the persons and their emails
+    """
+
+
+def add_point(name):
+    """
+    addong one docing point to the person
+    """
+
+
+def del_point(name):
+    """
+    deleting one point from the person
+    """
+
+
+def add_calender(calender_id):
+    """
+    adding the calender id to a file and saving it
+    """
+
+
+def get_calender():
+    """
+    gatting the calender id
+    """
+
+
+def add_non_activity(name):
+    """
+    adding the name of the activity to a file
+    """
+
+
+def del_non_activity(name):
+    """
+    removing the activity name from the files of activities that should not be documented
+    """
