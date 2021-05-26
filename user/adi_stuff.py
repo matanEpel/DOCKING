7d8a7bb2f85fcde9472219a28b9bef25c265a75e
matanepel <matan.epel12@gmail.com>
2021-05-26 21:39:09 +0300

    starting to work!

---
+++ user/adi_stuff.py
@@ -0,0 +1,5 @@
+# TODO: def add_doc(file_path)
+#  adds the dock to the drive and all the tags we need
+
+# TODO: def add_drive_data(drive_key)
+#  creates a connection to the data base (drive) based on the drive directory key
